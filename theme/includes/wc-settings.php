49edcd3db74b62e0dcdcb9afd9d5e3bef0ab35f7
Prystal <ali.axecorp@gmail.com>
2023-03-09 22:07:27 +0500

    Completed the payment functionality

--- theme/includes/wc-settings.php
+++ theme/includes/wc-settings.php
@@ -47,34 +47,33 @@ function shalior_flag_already_added_products_as_invalid ($result , $product_id){
     return $result;
 }
 
+// adds the product to cart on form submission
+function add_product_to_cart_on_form_submission( $entry, $form ) {
+    // Get the product ID from the hidden field
+    $product_id = rgar( $entry, '13' );
+    //var_dump($product_id);
+    // Debugging statement to check the product ID
+    $radio_button_value = rgar( $entry, '15');
 
-    function add_product_to_cart_on_form_submission( $entry, $form ) {
-        // Get the product ID from the hidden field
-        $product_id = rgar( $entry, '13' );
-        //var_dump($product_id);
-        // Debugging statement to check the product ID
-       
-    
-        // If the product ID is valid, add it to the cart
+    // If the product ID is valid, add it to the cart
 
-        if ( $product_id ) {
-            // Add the product to the cart
-            WC()->cart->add_to_cart( $product_id );
-    
-            // Debugging statement to check the cart contents
-            //error_log( 'Cart Contents: ' . print_r( WC()->cart->get_cart(), true ) );
-    
-            // Redirect the user to the cart page
-            // wp_redirect( wc_get_cart_url() );
-            // exit;
-        }
+    if ( $product_id ) {
+        // Add the product to the cart
+        WC()->cart->add_to_cart($product_id,1 ,"","",array('isExclusive'=>$radio_button_value));
+        //WC()->cart->add_to_cart($product_id);
+
+        // Debugging statement to check the cart contents
+        //error_log( 'Cart Contents: ' . print_r( WC()->cart->get_cart(), true ) );
+
+        // Redirect the user to the cart page
+        // wp_redirect( wc_get_cart_url() );
+        // exit;
     }
-    add_action( 'gform_after_submission_4', 'add_product_to_cart_on_form_submission', 10, 2 );
+}
+add_action( 'gform_after_submission_4', 'add_product_to_cart_on_form_submission', 10, 2 );
 
 
-
-
-
+// adds the product to user meta
 function add_product_id_to_user_meta( $order_id ) {
     // Get the current user ID
     $user_id = get_current_user_id();
@@ -102,10 +101,15 @@ function add_product_id_to_user_meta( $order_id ) {
 }
 
 
+
 add_action( 'woocommerce_checkout_update_order_meta', 'add_product_id_to_user_meta' );
 
 
 
+
+
+
+//checks if the user has purchased a product
 function has_purchased_product($user_id, $product_id) {
     // Get the purchased product IDs for the user
     $purchased_product_ids = get_user_meta($user_id, 'purchased_product_ids', true);
@@ -122,29 +126,37 @@ function has_purchased_product($user_id, $product_id) {
 
 
 
-// function update_product_price_on_form_submission( $entry, $form ){
-//     // Get the radio button value from the form submission
-//     $radio_button_value = rgar( $entry, '15' ); // replace input_1 with the ID of your radio button field
-//     // Check if the radio button value matches your desired value
-//     if ( $radio_button_value == 'non-ex') {
-//         // Loop through all products and update their prices
-//         $args = array(
-//             'post_type' => 'product',
-//             'posts_per_page' => -1,
-//         );
-//         $products = new WP_Query($args);
-//         foreach ( $products->posts as $product ) {
-//             // Get the product object
-//             $product_obj = wc_get_product( $product->ID );
-//             // Update the product price as desired
-//             $product_price = $product_obj->get_price();
-//             $new_price = $product_price - 1500; // replace with your desired price update
-//             $product_obj->set_price($new_price);
-//             $product_obj->save();
-//         }
-//     } 
-// }
+// updates the price
+add_action( 'woocommerce_before_calculate_totals', 'add_custom_price', 1000, 1);
+function add_custom_price( $cart ) {
+    // This is necessary for WC 3.0+
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) )
+        return;
+    // Avoiding hook repetition (when using price calculations for example | optional)
+    if ( did_action( 'woocommerce_before_calculate_totals' ) >= 2 )
+        return;
+    // Loop through cart items
+    foreach ( $cart->get_cart() as $cart_item ) {
+        if($cart_item['isExclusive'] == 'ex'){
+        }else{
+            $cart_item['data']->set_price( 500 );
+        }
+        
+    }
+}
 
-// add_action( 'gform_after_submission_4', 'update_product_price_on_form_submission', 10, 2);
+
+
+/**
+ * When an item is added to the cart, remove other products
+ */
+function woo_cart_ensure_only_one_item( $cart_contents ) {
+    return array( end( $cart_contents ) );
+}
+add_filter( 'woocommerce_cart_contents_changed', 'woo_cart_ensure_only_one_item' );
+
+
+
+
 
 ?>
